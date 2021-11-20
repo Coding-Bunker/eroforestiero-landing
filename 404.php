78ea98535caaaa7f977bc315222025776fde6f61
ludovicobesana <ludovicobesanaguide@gmail.com>
2021-11-20 14:44:22 +0100

    wip: Working on wp files

--- 404.php
+++ 404.php
@@ -1,20 +1,11 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	
-	<title><?php echo "404"?></title>
-	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+<?php get_header(); ?>
 
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php wp_head(); ?>
-</head>
+    <!-- Pagina 404 - Author: Andrea -->
+    <div class="error-page__image-container">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_page_not_found_su7k 1.png" alt="error-page image">
+        <div class="error-page__text-container">
+            <h3>Non siamo riusciti a trovare la pagina che stavi cercando</h3>
+        </div>
+    </div>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,48 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
+<!-- Definisce la lingua del sito web -->
 <html <?php language_attributes(); ?>>
-<head>
-	
-	<title>
-    <?php 
-        if (is_home()) {
-            echo "Homepage";
-        }
-        wp_title("", true, "right"); 
-    ?>
-    </title>
-	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <head>
+        <!-- Definisce il nome del sito, lo si cambia nelle impostazioni di WordPress -->
+        <title><?php bloginfo( 'name' ); wp_title(); ?></title>
+        <!-- Definisce la descrizione del sito, la si cambia nelle impostazioni di WordPress -->
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <!-- Definisce il charset del sito, lo si cambia nelle impostazioni di WordPress -->
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-  
-	<meta property="og:url" content="https://www.eroforestiero.org"  />
-	<meta property="og:title" content="Ero Forestiero — Imparare l'italiano si può ed è anche gratis!" />
-  <meta property="og:description" content="Ero Forestiero è un’associazione senza scopo di lucro, apartitica e 
-  apolitica che svolge attività formative a favore dell’integrazione sociale delle persone di origine non italiana 
-  presenti sul territorio di Vigevano, ponendo particolare attenzione alla tutela della loro identità culturale e religiosa." />
-	<meta property="og:image" content="https://i.imgur.com/sRcWZR7.png" />
-	
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/main.css" type="text/css" media="all" />
-    
-  <?php wp_head(); ?>
-</head>
+        <!-- ATTENZIONE: Nello sviluppo WordPress, per includere delle immagini o qualsiasi altro media o file presente nella cartella
+        del progetti è necessario inserire nell'attributo href o scr, a seconda del tag che si sta usando, prima
+        'echo get_stylesheet_directory_uri(); dentro i tag PHP di apertura e chiusura e poi inserire uno slash e il percorso dove si trova
+        il file che si sta includendo -->
 
-<body <?php body_class(); ?> >
-  
-<?php wp_body_open(); ?>
+        <!-- Definisce le favicon che ci saranno correlate al sito -->
+        <link href="" rel="shortcut icon" type="image/x-icon">
+        <link href="" rel="apple-touch-icon-precomposed">
+        <link href="" sizes="114x114" rel="apple-touch-icon-precomposed">
+        <link href="" sizes="72x72" rel="apple-touch-icon-precomposed">
+        <link href="" sizes="144x144" rel="apple-touch-icon-precomposed">
+        
+        <!-- Link a tutti i fogli di stile -->
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/main.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/404.css" type="text/css" media="all" />
 
-<!-- Navbar -->
-    <header class="website-header">
-        <div class="donate-div">
-            <p>Sostieni il progetto, dona il tuo cinque per mille dell'Irpef indicando il codice fiscale: <b>940205901181</b></p>
-        </div>
+		<!-- Stili e script di integrazione per Font Awesome -->
+        <script src="https://kit.fontawesome.com/0999994394.js" crossorigin="anonymous"></script>
+        <link rel=“stylesheet” href=“https://use.fontawesome.com/releases/v5.5.0/css/all.css” integrity=“sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU” crossorigin=“anonymous”>
 
-        <a href=""> <img src="https://www.countryflags.io/gb/flat/64.png" alt="English flag"> </a>
+        <link href="//db.onlinewebfonts.com/c/edeb336ecd776e23d99355eb2b52cbc7?family=NeuzeitGroteskW01-Regular" rel="stylesheet" type="text/css"/>
 
-        <div class="header-navbar noselect">
-            <span class="page-logo">Ero <span class="page-logo black-logo">Forestiero</span> </span>
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-            <a href="mailto:eroforestieroodv@gmail.com" class="button green-outline_button">Contattaci</a>
-        </div>
-    </header>
-<!-- End Navbar-->
+        <!-- Definisce l'head del sito -->
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+      <!-- Navbar -->
+          <header class="website-header">
+              <div class="donate-div">
+                  <p>Sostieni il progetto, dona il tuo cinque per mille dell'Irpef indicando il codice fiscale: <b>940205901181</b></p>
+              </div>
+
+              <a href=""> <img src="https://www.countryflags.io/gb/flat/64.png" alt="English flag"> </a>
+
+              <div class="header-navbar noselect">
+                  <span class="page-logo">Ero <span class="page-logo black-logo">Forestiero</span> </span>
+
+                  <a href="mailto:eroforestieroodv@gmail.com" class="button green-outline_button">Contattaci</a>
+              </div>
+          </header>
+      <!-- End Navbar-->
