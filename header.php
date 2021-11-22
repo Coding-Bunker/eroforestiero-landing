0af105aee05c3b7d5bc1108ddb2dad6723c31592
ludovicobesana <ludovicobesanaguide@gmail.com>
2021-11-22 13:04:29 +0100

    fix: Fixed font issue

--- header.php
+++ header.php
@@ -46,10 +46,9 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/404.css" type="text/css" media="all" />
 
 		<!-- Stili e script di integrazione per Font Awesome -->
-        <script src="https://kit.fontawesome.com/0999994394.js" crossorigin="anonymous"></script>
-        <link rel=“stylesheet” href=“https://use.fontawesome.com/releases/v5.5.0/css/all.css” integrity=“sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU” crossorigin=“anonymous”>
+        <!-- <script src="https://kit.fontawesome.com/0999994394.js" crossorigin="anonymous"></script>
+        <link rel=“stylesheet” href=“https://use.fontawesome.com/releases/v5.5.0/css/all.css” integrity=“sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU” crossorigin=“anonymous”> -->
 
-        <link href="//db.onlinewebfonts.com/c/edeb336ecd776e23d99355eb2b52cbc7?family=NeuzeitGroteskW01-Regular" rel="stylesheet" type="text/css"/>
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
